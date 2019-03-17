b88fb8a2dcd1c4d4672499b48a0ef64d795e944a
Adam Grabara <44620474+agrabara@users.noreply.github.com>
2019-03-17 19:05:35 +0100

    Working RoboArm steering, A decreases, B increases, AB changes servo.

--- main.ts
+++ main.ts
@@ -1,4 +1,86 @@
-basic.showIcon(IconNames.Duck)
+// Tutaj ustaw którymi złączkami S1-S8 masz masz podłączone serwa RoboRamienia
+// w przypadku płytki sterowniczej DFRobot Micro:bit Driver Expansion Board (v1.0) 
+// są to następujące łącza (w przypadku tej konfiguracji):
+// S1 - 15 (tutaj podłączona baza)
+// S2 - 14 (tutaj podłączone lewe serwo)
+// S3 - 13 (tutaj podłączone szczęki)
+// S4 - 12 (tutaj podłączone prawe serwo)
+// S5 - 11
+// S6 - 10
+// S7 -  9
+// S8 -  8
+
+const SERV_0 = 0
+const SERV_1 = 1
+const SERV_2 = 2
+const SERV_3 = 3
+const SERV_4 = 4
+const SERV_5 = 5
+const SERV_6 = 6
+const SERV_7 = 7
+const SERV_8 = 8
+const SERV_9 = 9
+const SERV_10 = 10
+const SERV_11 = 11
+const SERV_12 = 12
+const SERV_13 = 13
+const SERV_14 = 14
+const SERV_15 = 15
+
+// setup min and max numer of servos to be changed using AB buttons 
+const MAX_SERV = 15
+const MIN_SERV = 12
+
+// increase/decrease step
+const SERV_STEP = 10
+
+
+const SERV_P = SERV_12 //Prawy
+const SERV_S = SERV_13 //Szczęki
+const SERV_L = SERV_14 //Lewy
+const SERV_B = SERV_15 //Baza
+
+const MIN_SERV_P = 20
+const MAX_SERV_P = 140
+const MIN_SERV_L = 20
+const MAX_SERV_L = 100
+const MIN_SERV_S = 80
+const MAX_SERV_S = 100
+const MIN_SERV_B = 20
+const MAX_SERV_B = 150
+
+
+basic.showIcon(IconNames.Happy)
+
+// If you want Debug mode ON - press "AB" button during reboot
+let IsDebug: boolean
+if (input.buttonIsPressed(Button.AB)) IsDebug = true
+else IsDebug = false
+
+//IsDebug = true
+
+if (IsDebug) basic.showString("RamieRobota - Debug mode")
+
+let NumerSerwaL: number //Lewe serwo
+let KatSerwaL: number
+let NumerSerwaP: number //Prawe serwo
+let KatSerwaP: number
+let NumerSerwaS: number //Szczęki
+let KatSerwaS: number
+let NumerSerwaB: number //Baza
+let KatSerwaB: number
+
+// ustawiamy w pozycji "połowa" - przyda się w kalibracji
+// należy zdjąć ramię serwa i nałożyć je w mniej więcj połowie zakresu roboczego
+NumerSerwaL = SERV_L
+KatSerwaL = 20
+NumerSerwaP = SERV_P
+KatSerwaP = 20
+NumerSerwaS = SERV_S
+KatSerwaS = 80
+NumerSerwaB = SERV_B
+KatSerwaB = 90
+
 Servo.Servo(0, 90)
 Servo.Servo(1, 90)
 Servo.Servo(2, 90)
@@ -11,63 +93,111 @@ Servo.Servo(8, 90)
 Servo.Servo(9, 90)
 Servo.Servo(10, 90)
 Servo.Servo(11, 90)
-Servo.Servo(12, 90) //prawy   -  20->140
-Servo.Servo(13, 80) //szczęki -  80->100
-Servo.Servo(14, 90) //lewy    -  20->100
-Servo.Servo(15, 90) //baza
+Servo.Servo(SERV_P, KatSerwaP) //prawy   -  20->140
+Servo.Servo(SERV_S, KatSerwaS) //szczęki -  80->100
+Servo.Servo(SERV_L, KatSerwaL) //lewy    -  20->100
+Servo.Servo(SERV_B, KatSerwaB) //baza    -  20->150
 
-let NumerSerwa: number
-let KatSerwa: number
+let NumerSerwa = 13
+let KatSerwa = 90
 
-NumerSerwa = 13
-KatSerwa = 90
+basic.pause(2000)
+basic.clearScreen()
 
-input.onButtonPressed(Button.A, function () {
-    KatSerwa += 10
+// Increases angle of servo NumerSerwa by SERV_STEP
+input.onButtonPressed(Button.B, function () {
 
     switch (NumerSerwa) {
-        case 12:
-            if (KatSerwa > 140) KatSerwa = 20
+        case SERV_P:
+            KatSerwaP += SERV_STEP
+            if (KatSerwaP > MAX_SERV_P) KatSerwaP = MAX_SERV_P
+            if (IsDebug) basic.showNumber(Math.round(KatSerwaP / 10))
             break;
-        case 13:
-            if (KatSerwa > 100) KatSerwa = 80
+        case SERV_L:
+            KatSerwaL += SERV_STEP
+            if (KatSerwaL > MAX_SERV_L) KatSerwaL = MAX_SERV_L
+            if (IsDebug) basic.showNumber(Math.round(KatSerwaS / 10))
             break;
-        case 14:
-            if (KatSerwa > 100) KatSerwa = 20
+        case SERV_S:
+            KatSerwaS += SERV_STEP
+            if (KatSerwaS > MAX_SERV_S) KatSerwaS = MAX_SERV_S
+            if (IsDebug) basic.showNumber(Math.round(KatSerwaS / 10))
             break;
-        case 15:
-            if (KatSerwa > 150) KatSerwa = 30
-            break;
-
-        case 0:
+        case SERV_B:
+            KatSerwaB += SERV_STEP
+            if (KatSerwaB > MAX_SERV_B) KatSerwaB = MAX_SERV_B
+            if (IsDebug) basic.showNumber(Math.round(KatSerwaB / 10))
             break;
         default:
-            if (KatSerwa > 180) KatSerwa = 0
+            KatSerwa += SERV_STEP
+            if (KatSerwa > 180) KatSerwa = 180
+            if (IsDebug) basic.showNumber(Math.round(KatSerwa / 10))
+            Servo.Servo(NumerSerwa, KatSerwa)
     }
-    Servo.Servo(NumerSerwa, KatSerwa)
-    basic.showNumber(Math.round(KatSerwa / 10))
+    Servo.Servo(SERV_P, KatSerwaP) //prawy
+    Servo.Servo(SERV_S, KatSerwaS) //szczęki
+    Servo.Servo(SERV_L, KatSerwaL) //lewy
+    Servo.Servo(SERV_B, KatSerwaB) //baza
+
 })
 
-input.onButtonPressed(Button.B, function () {
-    NumerSerwa += 1
-    if (NumerSerwa > 15) NumerSerwa = 12
+// Decreases angle of servo NumerSerwa by SERV_STEP
+input.onButtonPressed(Button.A, function () {
+
     switch (NumerSerwa) {
-        case 12:
-            basic.showString("P")
+        case SERV_P:
+            KatSerwaP -= SERV_STEP
+            if (KatSerwaP < MIN_SERV_P) KatSerwaP = MIN_SERV_P
+            if (IsDebug) basic.showNumber(Math.round(KatSerwaP / 10))
             break;
-        case 13:
-            basic.showString("S")
+        case SERV_L:
+            KatSerwaL -= SERV_STEP
+            if (KatSerwaL < MIN_SERV_L) KatSerwaL = MIN_SERV_L
+            if (IsDebug) basic.showNumber(Math.round(KatSerwaL / 10))
             break;
-        case 14:
-            basic.showString("L")
+        case SERV_S:
+            KatSerwaS -= SERV_STEP
+            if (KatSerwaS < MIN_SERV_S) KatSerwaS = MIN_SERV_S
+            if (IsDebug) basic.showNumber(Math.round(KatSerwaS / 10))
             break;
-        case 15:
-            basic.showString("B")
-            break;
-        case 0:
+        case SERV_B:
+            KatSerwaB -= SERV_STEP
+            if (KatSerwaB < MIN_SERV_B) KatSerwaB = MIN_SERV_B
+            if (IsDebug) basic.showNumber(Math.round(KatSerwaB / 10))
             break;
         default:
-            basic.showNumber(NumerSerwa)
+            KatSerwa -= SERV_STEP
+            if (KatSerwa < 0) KatSerwa = 0
+            Servo.Servo(NumerSerwa, KatSerwa)
+            if (IsDebug) basic.showNumber(Math.round(KatSerwa / 10))
     }
+    Servo.Servo(SERV_P, KatSerwaP) //prawy
+    Servo.Servo(SERV_S, KatSerwaS) //szczęki
+    Servo.Servo(SERV_L, KatSerwaL) //lewy
+    Servo.Servo(SERV_B, KatSerwaB) //baza
+})
 
+
+
+input.onButtonPressed(Button.AB, function () {
+    NumerSerwa += 1
+    if (NumerSerwa > MAX_SERV) NumerSerwa = MIN_SERV
+
+    //if debug mode ON - display servo ID
+    switch (NumerSerwa) {
+        case SERV_P:
+            if (IsDebug) basic.showString("P")
+            break;
+        case SERV_S:
+            if (IsDebug) basic.showString("S")
+            break;
+        case SERV_L:
+            if (IsDebug) basic.showString("L")
+            break;
+        case SERV_B:
+            if (IsDebug) basic.showString("B")
+            break;
+        default:
+            if (IsDebug) basic.showNumber(NumerSerwa)
+    }
 })
