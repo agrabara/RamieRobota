7ae46206f78fe2d80c780b6c17370e2eabeecfcf
Adam Grabara <44620474+agrabara@users.noreply.github.com>
2019-03-23 12:03:49 +0100

    Updates.

--- main.ts
+++ main.ts
@@ -39,15 +39,10 @@ const SERV_B1 = SERV_0 //Biodro 1
 const SERV_K1 = SERV_7 //Kolano 1
 
 
-const MIN_SERV_P = 20
-const MAX_SERV_P = 140
-const MIN_SERV_L = 20
-
-const MAX_SERV_L = 100
-const MIN_SERV_S = 80
-const MAX_SERV_S = 100
-const MIN_SERV_B = 20
-const MAX_SERV_B = 150
+const MAX_SERV_B1 = 160
+const MIN_SERV_B1 = 20
+const MAX_SERV_K1 = 140
+const MIN_SERV_K1 = 20
 
 basic.showIcon(IconNames.Happy)
 
@@ -96,209 +91,60 @@ let KatSerwa = 90
 basic.pause(2000)
 basic.clearScreen()
 
-// Increases angle of servo NumerSerwa by SERV_STEP
-input.onButtonPressed(Button.B, function () {
 
-    switch (NumerSerwa) {
-        case SERV_P:
-            KatSerwaP += SERV_STEP
-            if (KatSerwaP > MAX_SERV_P) KatSerwaP = MAX_SERV_P
-            if (IsDebug) basic.showNumber(Math.round(KatSerwaP / 10))
-            break;
-        case SERV_L:
-            KatSerwaL += SERV_STEP
-            if (KatSerwaL > MAX_SERV_L) KatSerwaL = MAX_SERV_L
-            if (IsDebug) basic.showNumber(Math.round(KatSerwaL / 10))
-            break;
-        case SERV_S:
-            KatSerwaS += SERV_STEP
-            if (KatSerwaS > MAX_SERV_S) KatSerwaS = MAX_SERV_S
-            if (IsDebug) basic.showNumber(Math.round(KatSerwaS / 10))
-            break;
-        case SERV_B:
-            KatSerwaB += SERV_STEP
-            if (KatSerwaB > MAX_SERV_B) KatSerwaB = MAX_SERV_B
-            if (IsDebug) basic.showNumber(Math.round(KatSerwaB / 10))
-            break;
-        default:
-            KatSerwa += SERV_STEP
-            if (KatSerwa > 180) KatSerwa = 180
-            if (IsDebug) basic.showNumber(Math.round(KatSerwa / 10))
-            Servo.Servo(NumerSerwa, KatSerwa)
-    }
-    Servo.Servo(SERV_P, KatSerwaP) //prawy
-    Servo.Servo(SERV_S, KatSerwaS) //szczęki
-    Servo.Servo(SERV_L, KatSerwaL) //lewy
-    Servo.Servo(SERV_B, KatSerwaB) //baza
+function NogaPrzodGora() {
+    //basic.showString("G")
+    Servo.Servo(SERV_B1, MAX_SERV_B1)
+    Servo.Servo(SERV_K1, MAX_SERV_K1)
+    basic.pause(30)
+}
+function NogaTylDol() {
+    //basic.showString("D")
+    Servo.Servo(SERV_B1, MIN_SERV_B1)
+    Servo.Servo(SERV_K1, MIN_SERV_K1)
+    basic.pause(30)
+}
+function NogaTylGora() {
+    //basic.showString("TG")
+    Servo.Servo(SERV_B1, MIN_SERV_B1)
+    Servo.Servo(SERV_K1, MAX_SERV_K1)
+    basic.pause(30)
+}
+function NogaPrzodDol() {
+    //basic.showString("PD")
+    Servo.Servo(SERV_B1, MAX_SERV_B1)
+    Servo.Servo(SERV_K1, MIN_SERV_K1)
+    basic.pause(30)
+}
 
-})
-
-// Decreases angle of servo NumerSerwa by SERV_STEP
 input.onButtonPressed(Button.A, function () {
-
-    switch (NumerSerwa) {
-        case SERV_P:
-            KatSerwaP -= SERV_STEP
-            if (KatSerwaP < MIN_SERV_P) KatSerwaP = MIN_SERV_P
-            if (IsDebug) basic.showNumber(Math.round(KatSerwaP / 10))
-            break;
-        case SERV_L:
-            KatSerwaL -= SERV_STEP
-            if (KatSerwaL < MIN_SERV_L) KatSerwaL = MIN_SERV_L
-            if (IsDebug) basic.showNumber(Math.round(KatSerwaL / 10))
-            break;
-        case SERV_S:
-            KatSerwaS -= SERV_STEP
-            if (KatSerwaS < MIN_SERV_S) KatSerwaS = MIN_SERV_S
-            if (IsDebug) basic.showNumber(Math.round(KatSerwaS / 10))
-            break;
-        case SERV_B:
-            KatSerwaB -= SERV_STEP
-            if (KatSerwaB < MIN_SERV_B) KatSerwaB = MIN_SERV_B
-            if (IsDebug) basic.showNumber(Math.round(KatSerwaB / 10))
-            break;
-        default:
-            KatSerwa -= SERV_STEP
-            if (KatSerwa < 0) KatSerwa = 0
-            Servo.Servo(NumerSerwa, KatSerwa)
-            if (IsDebug) basic.showNumber(Math.round(KatSerwa / 10))
-    }
-    Servo.Servo(SERV_P, KatSerwaP) //prawy
-    Servo.Servo(SERV_S, KatSerwaS) //szczęki
-    Servo.Servo(SERV_L, KatSerwaL) //lewy
-    Servo.Servo(SERV_B, KatSerwaB) //baza
-})
-
-
-
-input.onButtonPressed(Button.AB, function () {
-    NumerSerwa += 1
-    if (NumerSerwa > MAX_SERV) NumerSerwa = MIN_SERV
-
-    //if debug mode ON - display servo ID
-    switch (NumerSerwa) {
-        case SERV_P:
-            if (IsDebug) basic.showString("P")
-            break;
-        case SERV_S:
-            if (IsDebug) basic.showString("S")
-            break;
-        case SERV_L:
-            if (IsDebug) basic.showString("L")
-            break;
-        case SERV_B:
-            if (IsDebug) basic.showString("B")
-            break;
-        default:
-            if (IsDebug) basic.showNumber(NumerSerwa)
+    for (let i = 0; i < 5; i++) {
+        NogaPrzodGora()
+        basic.pause(500)
+        NogaTylGora()
+        basic.pause(500)
+        NogaPrzodGora()
+        basic.pause(500)
+        NogaTylGora()
+        basic.pause(500)
     }
 })
-
-
-function RamieNaDol() {
-
-
-    for (let i = 0; i < 11; i++) {
-
-        KatSerwaP += SERV_STEP
-        if (KatSerwaP > MAX_SERV_P) KatSerwaP = MAX_SERV_P
-        if (IsDebug) basic.showNumber(Math.round(KatSerwaP / 10))
-        KatSerwaL -= SERV_STEP
-        if (KatSerwaL < MIN_SERV_L + 40) KatSerwaL = MIN_SERV_L + 40
-        if (IsDebug) basic.showNumber(Math.round(KatSerwaL / 10))
-        Servo.Servo(SERV_P, KatSerwaP) //prawy   -  20->140
-        Servo.Servo(SERV_L, KatSerwaL) //lewy    -  20->100
-        basic.pause(50)
+input.onButtonPressed(Button.B, function () {
+    for (let i = 0; i < 5; i++) {
+        NogaPrzodGora()
+        basic.pause(500)
+        NogaPrzodDol()
+        basic.pause(500)
+        NogaTylDol()
+        basic.pause(500)
+        NogaTylGora()
+        basic.pause(500)
     }
 
-}
-
-
-function RamieWGore() {
-
-
-    for (let i = 0; i < 11; i++) {
-
-        KatSerwaL += SERV_STEP
-        if (KatSerwaL > MAX_SERV_L) KatSerwaL = MAX_SERV_L
-        if (IsDebug) basic.showNumber(Math.round(KatSerwaL / 10))
-        KatSerwaP -= SERV_STEP
-        if (KatSerwaP < MIN_SERV_P) KatSerwaL = MIN_SERV_P
-        if (IsDebug) basic.showNumber(Math.round(KatSerwaP / 10))
-        Servo.Servo(SERV_P, KatSerwaP) //prawy   -  20->140
-        Servo.Servo(SERV_L, KatSerwaL) //lewy    -  20->100
-        basic.pause(50)
-    }
-
-}
-
-function SzczekiOtworz() {
-
-
-    for (let i = 0; i < 20; i++) {
-        KatSerwaS -= 1
-        if (KatSerwaS < MIN_SERV_S) KatSerwaS = MIN_SERV_S
-        if (IsDebug) basic.showNumber(Math.round(KatSerwaS / 10))
-        Servo.Servo(SERV_S, KatSerwaS) //szczęki -  80->100
-        basic.pause(50)
-    }
-
-}
-
-function SzczekiZamknij() {
-
-
-    for (let i = 0; i < 20; i++) {
-        KatSerwaS += 1
-        if (KatSerwaS > MAX_SERV_S) KatSerwaS = MAX_SERV_S
-        if (IsDebug) basic.showNumber(Math.round(KatSerwaS / 10))
-        Servo.Servo(SERV_S, KatSerwaS) //szczęki -  80->100
-        basic.pause(50)
-    }
-
-}
-
-function RamieWPrawo() {
-    for (let i = 0; i < 130; i++) {
-        KatSerwaB -= 1
-        if (KatSerwaB < MIN_SERV_B) KatSerwaB = MIN_SERV_B
-        if (IsDebug) basic.showNumber(Math.round(KatSerwaB / 10))
-        Servo.Servo(SERV_B, KatSerwaB) //baza    -  20->150
-        basic.pause(30)
-    }
-
-}
-
-function RamieWLewo() {
-    for (let i = 0; i < 130; i++) {
-        KatSerwaB += 1
-        if (KatSerwaB > MAX_SERV_B) KatSerwaB = MAX_SERV_B
-        if (IsDebug) basic.showNumber(Math.round(KatSerwaB / 10))
-        Servo.Servo(SERV_B, KatSerwaB) //baza    -  20->150
-        basic.pause(30)
-    }
-
-}
-
-
+})
 input.onGesture(Gesture.Shake, function () {
 
-    RamieWPrawo()
-})
 
-input.onGesture(Gesture.ScreenDown, function () {
-
-    RamieWLewo()
 })
 
 
-input.onGesture(Gesture.LogoDown, function () {
-
-    SzczekiOtworz()
-})
-
-
-input.onGesture(Gesture.LogoUp, function () {
-
-    SzczekiZamknij()
-})
